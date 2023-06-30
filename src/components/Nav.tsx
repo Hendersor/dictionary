0cf9d9f56c2f7c36056b638154725a0f268bcb6d
Francisco <montejof1998@gmail.com>
2023-06-29 21:12:22 -0700

    responsive design ready

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,8 +4,8 @@ import {FiSearch} from 'react-icons/fi'
 
 const Nav = () => {
      return(
-          <div className='w-full h-36 flex flex-col  items-center px-4'>
-               <div className='flex justify-between w-full h-1/2'>
+          <div className='w-full h-36 flex flex-col items-center px-4 md:px-8 md:h-44'>
+               <div className='max-w-3xl flex justify-between w-full h-1/2 lg:w-4/5'>
                     <figure className='w-full h-full flex items-center'>
                          <BiBookAlt className='text-5xl text-slate-600'/>
                     </figure>
@@ -13,8 +13,8 @@ const Nav = () => {
 
 
 
-               <div className='w-full h-1/2 flex items-center justify-center'>
-                    <div className='rounded-3xl bg-[#f8eeee] w-full h-3/4 flex items-center justify-center px-4'>
+               <div className='w-full h-1/2 max-w-3xl flex items-center justify-center lg:w-4/5'>
+                    <div className='rounded-3xl max-w-3xl bg-[#f8eeee] w-full h-3/4 flex items-center justify-center px-4 md:rounded-2xl'>
                          <input type="text" className='w-full h-full rounded-3xl bg-[#f8eeee] ' placeholder='Search'/>
                          <FiSearch className='text-2xl text-[#bd96d9]'/>
                     </div>
--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -3,13 +3,13 @@ import {ImPlay3} from 'react-icons/im'
 
 const Word = () => {
      return(
-          <div className='w-full h-20 flex justify-between items-center px-4'>
+          <div className='w-full h-20 max-w-3xl flex justify-between items-center px-4 md:px-8 md:my-10 lg:w-4/5 lg:h-28 lg:my-10 lg:mx-auto'>
                <div className='h-full w-1/2 flex flex-col content-center justify-center'>
-                    <h1 className='font-bold text-3xl'>keyboard</h1>
-                    <p className='text-[#a645ef]'>/ˈkiːbɔːd/</p>
+                    <h1 className='font-bold text-3xl md:text-6xl md:my-3'>keyboard</h1>
+                    <p className='text-[#a645ef] md:text-2xl'>/ˈkiːbɔːd/</p>
                </div>
-               <figure className='bg-[#eacffc] h-11 w-11 rounded-full flex items-center justify-center cursor-pointer'>
-                    <ImPlay3 className='text-[#a645ef]'/>
+               <figure className='bg-[#eacffc] h-11 w-11 rounded-full flex items-center justify-center cursor-pointer md:h-20 md:w-20'>
+                    <ImPlay3 className='text-[#a645ef] md:text-3xl'/>
                </figure>
           </div>
      )
